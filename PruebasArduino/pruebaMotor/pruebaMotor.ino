b1aa60059fcde733c1c538e7f3b1d35a0edcbb83
SerjAlva <sergio-d.h.a@hotmail.com>
2018-06-06 00:36:10 -0500

    bug fixed

--- PruebasArduino/pruebaMotor/pruebaMotor.ino
+++ PruebasArduino/pruebaMotor/pruebaMotor.ino
@@ -4,7 +4,7 @@ SoftwareSerial blue(2,3); //(Rx, Tx)
 char nombre[21] = "CrustaceoMovil";
 char BPS = '4'; // 1 -> 1200, 2 -> 2400, n -> 1200*(2^(n-1))
 char PWD[5] = "0000";
-int state = 0;
+char state;
 int en1M1 = 6;
 int en2M1 = 7;
 int en1M2 = 8;
@@ -47,31 +47,47 @@ void setup() {
 }
 
 void loop() {
-  if(Serial.available() > 0){
-    state = Serial.read();
-  }
-  if(state == 0){
-    analogWrite(velM1, 254);
-    analogWrite(velM2, 254);
-    digitalWrite(en1M1, HIGH);
-    digitalWrite(en2M1, LOW);
-    digitalWrite(en1M2, HIGH);
-    digitalWrite(en2M2, LOW);
-  }
-  else if(state == 1){
-    analogWrite(velM1, 254);
-    analogWrite(velM2, 254);
-    digitalWrite(en2M1, HIGH);
-    digitalWrite(en1M1, LOW);
-    digitalWrite(en2M2, HIGH);
-    digitalWrite(en1M2, LOW);
-  }
-  else if(state == 2){
-    analogWrite(velM1, 254);
-    analogWrite(velM2, 157);
-  }
-  else if(state == 3){
-    analogWrite(velM1, 157);
-    analogWrite(velM2, 254);  
+  while(blue.available()){
+    state = blue.read();
+    switch(state){
+      case '0':
+        blue.println("Avanzando");
+        analogWrite(velM1, 254);
+        analogWrite(velM2, 254);
+        digitalWrite(en1M1, HIGH);
+        digitalWrite(en2M1, LOW);
+        digitalWrite(en1M2, HIGH);
+        digitalWrite(en2M2, LOW);
+      break;
+      case '1':
+        blue.println("Retrocediendo");
+        analogWrite(velM1, 254);
+        analogWrite(velM2, 254);
+        digitalWrite(en2M1, HIGH);
+        digitalWrite(en1M1, LOW);
+        digitalWrite(en2M2, HIGH);
+        digitalWrite(en1M2, LOW);
+      break;
+      case'2':
+        blue.println("Girando a la derecha");
+        analogWrite(velM1, 254);
+        analogWrite(velM2, 157);
+      break;
+      case '3':
+        blue.println("Girando a la izquierda");
+        analogWrite(velM1, 157);
+        analogWrite(velM2, 254);  
+      break;
+      case '4':
+        blue.println("Me estoy quemando!! :'v");
+        analogWrite(velM1, 254);
+        analogWrite(velM2, 254);
+        digitalWrite(en1M1, HIGH);
+        digitalWrite(en2M1, LOW);
+        digitalWrite(en1M2, LOW);
+        digitalWrite(en2M2, HIGH);
+      break;
+    }
+    
   }
 }
