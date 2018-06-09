8d74fcde0c2de3f299920766316f94485391c4f6
Alpercon <srlobato@comunidad.unam.mx>
2018-06-09 13:12:22 -0500

    Esqueleto de la app listo

--- AppAndroid/Arduino/app/src/main/java/com/example/epsilon/arduino/MainActivity.java
+++ AppAndroid/Arduino/app/src/main/java/com/example/epsilon/arduino/MainActivity.java
@@ -1,29 +1,51 @@
 package com.example.epsilon.arduino;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    int REQUEST_ENABLE_BT = 17;
+    //Codigo para las cards del inicio
+    private RecyclerView recyclerViewCard;
+    private RecyclerViewAdaptador adaptadorCard;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Codigo para las cards del inicio
+        recyclerViewCard = (RecyclerView) findViewById(R.id.recyclerMode);
+        recyclerViewCard.setLayoutManager(new LinearLayoutManager(this));
+        adaptadorCard = new RecyclerViewAdaptador(obtenerCards());
+        recyclerViewCard.setAdapter(adaptadorCard);
 
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
-            // Device does not support Bluetooth
-            Toast.makeText(getApplicationContext(), "El dispositivo no soporta Bluetooth", Toast.LENGTH_SHORT).show();
-        }
-
-        if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
     }
+
+
+    //Codigo para las cards del inicio
+    public List<CardModelo> obtenerCards(){
+        List<CardModelo> card = new ArrayList<>();
+        card.add(new CardModelo("Manual","Controla tu mismo este poderoso auto!","Quiero manejarlo ya!",R.drawable.auto));
+        card.add(new CardModelo("Automatico","Cansado de conducir? Prueba los moddos automaticos","Llevame a ellos!",R.drawable.auto2));
+        return  card;
+    }
+
+
+
+
 }
---
+++ AppAndroid/Arduino/app/src/main/java/com/example/epsilon/arduino/RecyclerViewAdaptador.java
@@ -0,0 +1,84 @@
+package com.example.epsilon.arduino;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.List;
+
+public class RecyclerViewAdaptador extends RecyclerView.Adapter<RecyclerViewAdaptador.ViewHolder> implements View.OnClickListener {
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView titulo,descripcion;
+        Button boton;
+        ImageView fotoAuto;
+        Context context;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            context = itemView.getContext();
+            titulo = (TextView)itemView.findViewById(R.id.tituloCard);
+            descripcion = (TextView)itemView.findViewById(R.id.descripcionCard);
+            boton = (Button)itemView.findViewById(R.id.botonCard);
+            fotoAuto = (ImageView)itemView.findViewById(R.id.imgAuto);
+        }
+       void setOnClickListeners (){
+            boton.setOnClickListener(this);
+       }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.botonCard:
+                    if(titulo.getText() == "Manual") {
+                        Intent intent = new Intent(context, Manual.class);
+                        context.startActivity(intent);
+                        break;
+                    }else{
+                        Intent intent = new Intent(context, ModosAutomaticos.class);
+                        context.startActivity(intent);
+                        break;
+                    }
+            }
+        }
+    }
+
+    public List<CardModelo> cardLista;
+
+    public RecyclerViewAdaptador(List<CardModelo> cardLista) {
+        this.cardLista = cardLista;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card,parent,false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.titulo.setText(cardLista.get(position).getTitulo());
+        holder.descripcion.setText(cardLista.get(position).getDescripcion());
+        holder.boton.setText(cardLista.get(position).getBoton());
+        holder.fotoAuto.setImageResource(cardLista.get(position).getFotoAuto());
+
+        holder.setOnClickListeners();
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return cardLista.size();
+    }
+}
